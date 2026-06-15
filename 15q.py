d47c83c9ebec011a6b3f7f76d8bc6ad84332c7a4
Sunil Kumar I S <sunilkumarjs4155.sse@saveetha.com>
2026-06-15 10:29:58 +0530

    Add files via upload

---
+++ 15q.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3)
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+print("Accuracy:", clf.score(X_test, y_test))
